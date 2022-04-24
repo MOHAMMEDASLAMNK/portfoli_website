c73bd7f02ef8ab56d62102e76593e6d66220a81e
Mohammed Aslam NK <44094763+MOHAMMEDASLAMNK@users.noreply.github.com>
2022-04-24 07:37:33 +0530

    REL

--- assets/js/project.js
+++ assets/js/project.js
@@ -7,7 +7,7 @@ const projectcards = document.querySelector(".projectcards");
 const projects = [
   {
     title: "Quiz App",
-    cardImage: "assets/images/project-page/quiz.jpg",
+    cardImage: "assets/images/project-page/ESP32 RELAY PCB.jpg",
     description: "A PCB designed for contolling devices mannually and through cloud.",
     tagimg: "https://cdn.iconscout.com/icon/free/png-512/react-1-282599.png",
     Previewlink: "",
