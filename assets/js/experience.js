4983b591cba438d781fc6463054010cac9957cbd
Mohammed Aslam NK <44094763+MOHAMMEDASLAMNK@users.noreply.github.com>
2022-04-24 07:05:14 +0530

    Update experience.js

--- assets/js/experience.js
+++ assets/js/experience.js
@@ -6,7 +6,7 @@ const experiencecards = document.querySelector(".experience-cards");
 const exp = [
   {
     title: "Embedded System & ioT Intern",
-    cardImage: "assets/images/experience-page/nctf.jpg",
+    cardImage: "assets/images/experience-page/nctf.jfif",
     place: "National Council for Technology and Training",
     time: "(May, 2020 - present)",
     desp: "<li>Worked to migrate Flipkart lite’s product page to AMP pages so that they load up instantly.</li> <li>Made changes in the current progressive web app of Flipkart to react to different actions performed on AMP page.</li> <li>Created a node module called “fk-amp” which abstracts all the files and middlewares and can be easily imported and used from express server.</li>",
